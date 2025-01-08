b32aea13a864ec9f4daa8b1cbdcba7de23282b99
Akshitha Reddy Paidi <akshithareddy.paidi@gmail.com>
2025-01-08 12:58:28 +0530

    added new files to repo

---
+++ function.js
@@ -0,0 +1,19 @@
+//! witjout parameters & arguments
+// function demo(){    // func declare
+//     console.log("hello");
+// }
+// demo(); //func call
+
+//! with para & arg
+
+// function add1(a,b){
+//     console.log(a+b);
+// }
+// add1(5,6); 
+// add1(7,8);
+// add1(3,4);
+
+function addtoCart(){
+    document.body.style.background="teal";
+    alert("Product added to cart1");
+}
--- loopstatements.js
+++ loopstatements.js
@@ -1,5 +1,46 @@
-console.log("Hello world!");
-console.log("Hello world!");
-console.log("Hello world!");
-console.log("Hello world!");
-console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+// console.log("Hello world!");
+
+// for(let i=1;i<=5;i++){
+//     console.log("Hello world!");
+// }
+// let n=100;
+// for(let i=1;i<=n;i++){
+//     //console.log(i);
+
+//     if(i%2 ==0){
+//         console.log(i+ "is an even number");
+//     } else{
+//         console.log(i+" is an odd number");
+//     }
+// }
+
+
+//! write a js program to find a factorial of number
+//! ex:  5=120; 5*4*3*2*1
+
+let n=5;
+let factorial=1;
+// for(let i=1;i<=n;i++){
+//     factorial*=i;
+//     // console.log(i);
+//     // console.log(factorial);
+    
+//     }
+    // console.log("Factorial of number is "+factorial);
+//! while loop
+// let i=1;
+// while(i<=5){
+//     console.log("good morning");
+//     i++;
+// }
+
+//! do-while
+let i = 1;
+do{console.log("Wake up");
+    i++;
+}
+    while(i<=5);
