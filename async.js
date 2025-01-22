9b690c2d6c4f975146e870f8425ca13d423f86ba
AkshithaPaidi <168356735+AkshithaPaidi@users.noreply.github.com>
2025-01-22 10:30:52 +0530

    api fetching methods added

--- async.js
+++ async.js
@@ -1,3 +1,35 @@
+
+function fetchUsers(){
+    let res= window.fetch("https://jsonplaceholder.typicode.com/users");
+    console.log(res);
+
+    res.then(val =>{
+        // console.log(val);
+        // console.log(val.json());
+        return val.json().then(data=>{
+            // console.log(data);
+            let store=document.getElementById("store");
+            data.map((user)=>{
+                // console.log(user);
+                store.innerHTML+= `
+                <tr>
+                <td>${user.id}</td>
+                <td>${user.name}</td>
+                <td>${user.email}</td>
+                <td>${user.company.name}</td>
+                </tr>
+                `
+            })
+        })
+    }).catch(err=>console.log(err))
+}
+fetchUsers();
+
+
+
+
+
+
 // console.log(10);
 // console.log(20);
 // setTimeout(()=>{
@@ -14,17 +46,19 @@
 // let p1=new Promise((resolve,reject)=>{}); 
 // console.log(p1);
 
-let p2=new Promise((resolve,reject)=>{
-    resolve("success");
-});
-console.log(p2);
-p2.then((response)=>{
-    console.log(response);
-}).catch((error)=>{
-    console.log(error);
-}).finally(()=>console.log("finally printing both"))
+// let p2=new Promise((resolve,reject)=>{
+//     resolve("success");
+// });
+// console.log(p2);
+// p2.then((response)=>{
+//     console.log(response);
+// }).catch((error)=>{
+//     console.log(error);
+// }).finally(()=>console.log("finally printing both"))
 
 // let p3= new Promise((resolve,reject)=>{
 //     reject("failure");
 // });
 // console.log(p3);
+
+
