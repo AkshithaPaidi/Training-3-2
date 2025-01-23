9875eac513d167a733eb789d88bd20c382fb1462
AkshithaPaidi <168356735+AkshithaPaidi@users.noreply.github.com>
2025-01-23 10:32:22 +0530

    dom files added

--- async.js
+++ async.js
@@ -1,29 +1,29 @@
 
-function fetchUsers(){
-    let res= window.fetch("https://jsonplaceholder.typicode.com/users");
-    console.log(res);
+// function fetchUsers(){
+//     let res= window.fetch("https://jsonplaceholder.typicode.com/users");
+//     console.log(res);
 
-    res.then(val =>{
-        // console.log(val);
-        // console.log(val.json());
-        return val.json().then(data=>{
-            // console.log(data);
-            let store=document.getElementById("store");
-            data.map((user)=>{
-                // console.log(user);
-                store.innerHTML+= `
-                <tr>
-                <td>${user.id}</td>
-                <td>${user.name}</td>
-                <td>${user.email}</td>
-                <td>${user.company.name}</td>
-                </tr>
-                `
-            })
-        })
-    }).catch(err=>console.log(err))
-}
-fetchUsers();
+//     res.then(val =>{
+//         // console.log(val);
+//         // console.log(val.json());
+//         return val.json().then(data=>{
+//             // console.log(data);
+//             let store=document.getElementById("store");
+//             data.map((user)=>{
+//                 // console.log(user);
+//                 store.innerHTML+= `
+//                 <tr>
+//                 <td>${user.id}</td>
+//                 <td>${user.name}</td>
+//                 <td>${user.email}</td>
+//                 <td>${user.company.name}</td>
+//                 </tr>
+//                 `
+//             })
+//         })
+//     }).catch(err=>console.log(err))
+// }
+// fetchUsers();
 
 
 
@@ -60,5 +60,26 @@ fetchUsers();
 //     reject("failure");
 // });
 // console.log(p3);
+// let p= new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve("success");
+//     },3000)
+
+// })
+// async function demo(){
+//     console.log("Start");
+//     let x=await p;
+    
+//     console.log(x);
+//     console.log("end");
+
+// }
+// demo();
 
 
+async function fetchUsers() {
+    let response= await fetch("https://jsonplaceholder.typicode.com/users");
+    let data= await response.json();
+    console.log(data);
+}
+fetchUsers();
---
+++ dom.js
@@ -0,0 +1,7 @@
+// document.getElementById("demo");
+let ele=document.getElementById("demo");
+ele.innerText="DOM";
+console.log(ele);
+let div=document.getElementById("test");
+div.innerHTML="<h1>Header</h1>"
+console.log(div);
