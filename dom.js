2d17857be7d996a71bab3bf23592d91ceaec68f1
AkshithaPaidi <168356735+AkshithaPaidi@users.noreply.github.com>
2025-01-24 15:32:10 +0530

    added event handler files

--- dom.js
+++ dom.js
@@ -1,7 +1,70 @@
-// document.getElementById("demo");
-let ele=document.getElementById("demo");
-ele.innerText="DOM";
-console.log(ele);
-let div=document.getElementById("test");
-div.innerHTML="<h1>Header</h1>"
-console.log(div);
+// // document.getElementById("demo");
+// let ele=document.getElementById("demo");
+// ele.innerText="DOM";
+// console.log(ele);
+// let div=document.getElementById("test");
+// div.innerHTML="<h1>Header</h1>"
+// console.log(div);
+// let ele=document.getElementsByClassName("test");
+// // console.log(ele);
+// // console.log(Array.isArray(ele));
+// ele[0].style.backgroundColor="yellow";
+// //! spread operator
+// let x=[...ele];
+// console.log(x , Array.isArray(x));
+// x.map((element)=>{
+//     // console.log(Element);
+//     element.style.backgroundColor="teal";
+// })
+
+// let ele =document.getElementsByTagName("div");
+// console.log(ele);
+
+// let ele =document.getElementsByName("div");
+// [...ele].map(element =>{
+//     // console.log(ele);
+//     element.style.backgroundColor="aqua";
+// })
+
+// let ele =document.querySelector("demo");
+// console.log(ele);
+
+
+// let ele = document.querySelectorAll(".test");
+// // console.log(ele);
+// [...ele].map(element=>{
+//     console.log(element.innerText);
+// })
+
+
+// ! events:
+// let btn=document.getElementById(btn);
+// // btn.addEventListener("events",callbackfn)
+// btn.addEventListener("click",()=>{
+//     console.log("button clicked");
+//     document.body.style.background="dodgerblue"
+// })
+
+// btn.addEventListener("mousehover",()=>{
+//     console.log("mouse");
+//     document.body.style.background="dodgerblue"
+// })
+
+// btn.addEventListener("mouseleave",()=>{
+//     console.log("mouseleave");
+//     document.body.style.background="tomato"
+// })
+
+// let inp=document.querySelector("#inp");
+// inp.addEventListener("keydown",()=>{
+//     console.log("key pressed down");
+// })
+
+// inp.addEventListener("keyup",()=>{
+//     console.log("key up");
+// })
+
+
+
+
+
---
+++ event.js
@@ -0,0 +1,21 @@
+// Add event listeners for each text
+document.getElementById("Thistle").addEventListener("mouseover", () => {
+    document.body.style.background = "Thistle";
+});
+
+document.getElementById("LightPink").addEventListener("mouseover", () => {
+    document.body.style.background = "LightPink";
+});
+
+document.getElementById("LightSteelBlue").addEventListener("mouseover", () => {
+    document.body.style.background = "LightSteelBlue";
+});
+
+
+document.getElementById("Gray").addEventListener("mouseover", () => {
+    document.body.style.background = "Gray";
+});
+
+document.getElementById("MediumAquamarine").addEventListener("mouseover", () => {
+    document.body.style.background = "MediumAquamarine";
+});
