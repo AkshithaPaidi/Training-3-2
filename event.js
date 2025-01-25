51ed5688df032924c08d0e675729cd161fe8496d
AkshithaPaidi <168356735+AkshithaPaidi@users.noreply.github.com>
2025-01-25 14:30:44 +0530

    Add files via upload
    uploaded QR generator file

--- event.js
+++ event.js
@@ -1,21 +1,33 @@
-// Add event listeners for each text
-document.getElementById("Thistle").addEventListener("mouseover", () => {
-    document.body.style.background = "Thistle";
-});
+// // Add event listeners for each text
+// document.getElementById("Thistle").addEventListener("mouseover", () => {
+//     document.body.style.background = "Thistle";
+// });
 
-document.getElementById("LightPink").addEventListener("mouseover", () => {
-    document.body.style.background = "LightPink";
-});
+// document.getElementById("LightPink").addEventListener("mouseover", () => {
+//     document.body.style.background = "LightPink";
+// });
 
-document.getElementById("LightSteelBlue").addEventListener("mouseover", () => {
-    document.body.style.background = "LightSteelBlue";
-});
+// document.getElementById("LightSteelBlue").addEventListener("mouseover", () => {
+//     document.body.style.background = "LightSteelBlue";
+// });
 
 
-document.getElementById("Gray").addEventListener("mouseover", () => {
-    document.body.style.background = "Gray";
-});
+// document.getElementById("Gray").addEventListener("mouseover", () => {
+//     document.body.style.background = "Gray";
+// });
 
-document.getElementById("MediumAquamarine").addEventListener("mouseover", () => {
-    document.body.style.background = "MediumAquamarine";
-});
+// document.getElementById("MediumAquamarine").addEventListener("mouseover", () => {
+//     document.body.style.background = "MediumAquamarine";
+// });
+
+let bgColor=document.querySelectorAll(".bgcolor");
+[...bgColor].map((ele)=>{
+    ele.addEventListener("mouseover",()=>{
+        // console.log(ele.innerText);
+        ele.style.backgroundColor=ele.innerText;
+
+    });
+    ele.addEventListener("mouseleave",()=>{
+        ele.style.backgroundColor="transparent";
+    })
+})
